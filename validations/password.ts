bad149891f550ed20d6007b90924199564b59b16
Kareem Ghorab <kareemghorab3@gmail.com>
2023-10-10 18:18:00 +0300

    [+] DB

---
+++ validations/password.ts
@@ -0,0 +1,4 @@
+export const passwordRegex =
+	/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/
+export const passwordError =
+	'Password must contain a minimum of eight characters, at least one letter and one number'
