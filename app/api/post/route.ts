053d31a3a98b0d5ac6197a211257f6485b630066
Kareem Ghorab <kareemghorab3@gmail.com>
2023-10-11 23:08:57 +0300

    [*] Post Api endpoint

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -1,6 +1,10 @@
-import { getAllPosts } from '@/service/server/post'
+import { getFeed } from '@/service/server/post'
 
 export async function GET(request: Request) {
-	const data = await getAllPosts()
+	const { page = 1, userId } = await request.json()
+	if (!userId) throw new Error('User ID required')
+
+	const data = await getFeed(userId, page)
+
 	return Response.json(data)
 }
