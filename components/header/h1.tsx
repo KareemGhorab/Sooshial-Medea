55a46708c2ca77912d9e7ecf595d7d4340d1dcc2
Kareem Ghorab <kareemghorab3@gmail.com>
2023-10-12 23:34:06 +0300

    [*] Main title responsiveness

--- components/header/h1.tsx
+++ components/header/h1.tsx
@@ -6,7 +6,14 @@ type Props = {
 }
 
 const H1: React.FC<Props> = ({ title }): JSX.Element => (
-	<h1 className={clsx(cabin.className, 'text-7xl font-bold')}>{title}</h1>
+	<h1
+		className={clsx(
+			cabin.className,
+			'text-2xl sm:text-3xl md:text-4xl lg:text-6xl xl:text-7xl font-bold'
+		)}
+	>
+		{title}
+	</h1>
 )
 
 export default H1
