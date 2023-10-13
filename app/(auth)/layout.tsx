e6d169a324db6de05dcf5bea1df6b27f9a3dc420
Kareem Ghorab <kareemghorab3@gmail.com>
2023-10-13 19:25:02 +0300

    [-] BE

--- app/(auth)/layout.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import AuthProvider from '@/context/auth-provider'
-
-const AuthLayout: React.FC<React.PropsWithChildren> = ({ children }) => (
-	<AuthProvider>{children}</AuthProvider>
-)
-export default AuthLayout
