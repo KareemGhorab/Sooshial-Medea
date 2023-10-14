cb1c38efc0ddab1f7c654c33e6b0ed7f239f8e2f
Kareem Ghorab <kareemghorab3@gmail.com>
2023-10-14 08:30:29 +0300

    [*] Chores

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -6,24 +6,20 @@ import { ToastContainer, toast } from 'react-toastify'
 import { PiSpinnerGapThin } from 'react-icons/pi'
 
 import { TPost } from '@/types/app'
-import { getAllPosts } from '@/service/client/post'
+import { getPosts } from '@/service/post'
 import { toastOptions } from '@/utils/toast'
 
 import PostList from './posts/post-list'
-import { useSession } from 'next-auth/react'
 
 const Feed: React.FC = (): JSX.Element => {
 	const [posts, setPosts] = useState<TPost[]>([])
 	const [loading, setLoading] = useState<boolean>(true)
 	const [pageNumber, setPageNumber] = useState<number>(1)
-	const { data } = useSession()
-
-	console.log({ data })
 
 	useEffect(() => {
 		setLoading(true)
-		getAllPosts(pageNumber, data?.user?.email ?? null)
-			.then(({ data }) => setPosts((op) => [...op, ...data.posts]))
+		getPosts(pageNumber, '')
+			.then((data) => setPosts((op) => [...op, ...data]))
 			.catch((err: AxiosError) => {
 				if (err.response?.status === 404) return
 				toast.error(err?.message, toastOptions)
---
+++ service/post.ts
@@ -0,0 +1,12 @@
+import { TPost } from '@/types/app'
+
+export const getPosts = async (
+	page: number,
+	username: string
+): Promise<TPost[]> => {
+	return []
+}
+
+export const addPost = async (username: string): Promise<void> => {
+	return
+}
